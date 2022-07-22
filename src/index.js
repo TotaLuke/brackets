f0b4451c52261483d43bc07909070b6e20673fec
TotaLukem <nickola.anelka@gmail.com>
2022-07-22 08:32:18 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let head = 0;
+  while ( head < str.length) {
+    bracketsConfig.forEach(elem => {
+      if (str[head] === elem[0] && str[head + 1] === elem[1]) {
+        str = str.slice(0, head) + str.slice(head + 2, str.length);
+        head = head - 2;
+      }
+    })
+    head++;
+  }
+  return (head === 0);
 }
